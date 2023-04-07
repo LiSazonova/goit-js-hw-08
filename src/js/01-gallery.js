9658c932841e9d9c57aff0bb59f654db2bd09508
LiSazonova <lilissazonova@gmail.com>
2023-04-07 21:50:05 +0300

    add task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,38 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log("galleryItems");
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const addGalleryItems = galleryCreateMarkup(galleryItems);
+
+galleryEl.innerHTML = addGalleryItems;
+galleryEl.addEventListener('click', onGalleryElClick);
+
+function galleryCreateMarkup(items) {
+    return items
+        .map(({ preview, original, description }) =>
+        `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+    `)
+        .join('');
+}
+
+function onGalleryElClick(evt) {
+    evt.preventDefault();
+
+    let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+}
