0c6a68c6e426f6fe57239bbffc91131e994630d7
LiSazonova <lilissazonova@gmail.com>
2023-04-08 18:35:54 +0300

    add task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const DATA_TIME_STORAGE_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+onResumeVideo();
+
+player.on('timeupdate', throttle(setDataTime, 500));
+
+function setDataTime(data) {
+  localStorage.setItem(DATA_TIME_STORAGE_KEY, data.seconds);
+}
+
+function onResumeVideo() {
+  const currentVideoplayerTime = localStorage.getItem(DATA_TIME_STORAGE_KEY);
+
+  if (currentVideoplayerTime) {
+    player.setCurrentTime(currentVideoplayerTime);
+  }
+}
